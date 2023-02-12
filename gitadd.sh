c74402526f107db2096b5ee218601ef361b5437d
powwu <hazy@vivaldi.net>
2023-02-12 16:41:37 -0500

    okay

--- gitadd.sh
+++ gitadd.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-git add gitadd.sh .gitignore bin README.md sources white.png wallpapers/albums wallpapers/favorites wallpapers/firewatch wallpapers/geometric wallpapers/keyborb/*.png wallpapers/keyborb/*.jpg wallpapers/keyborb/*.jpeg wallpapers/minimal wallpapers/miscdump
+git add .
+git reset wallpapers/keyborb/sus/
 git commit -a
